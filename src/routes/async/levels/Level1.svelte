b24e31185cf3b2e164396be583d5d8885aeb14a8
Levi Jones <leviwill.j@gmail.com>
2023-10-20 22:26:06 -0600

    TEMP

--- src/routes/async/levels/Level1.svelte
+++ src/routes/async/levels/Level1.svelte
@@ -3,6 +3,17 @@
         <p>Buckle up kiddo, time to learn</p>
     </div>
     <div class="puzzle">
-        <p>this is the puzzle</p>
+        <p>// Pull data from the dog.ceo api</p>
+        <p>
+            <span>const stream = </span>
+            <span class="drop-spot"></span>
+            <span class="drop-spot"></span>
+        </p>
+        <p>
+            <span>const data =</span>
+        </p>
+        <p>
+            <span>return data</span>
+        </p>
     </div>
 </div>
