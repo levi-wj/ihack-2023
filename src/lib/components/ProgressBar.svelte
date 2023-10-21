09bfb8dadffa09a51ef3497567dbcaa006e64aee
emmaohn <joh20123@byui.edu>
2023-10-21 00:28:51 -0600

    progress bar

---
+++ src/lib/components/ProgressBar.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let toComplete;
+    export let completed;
+
+    let progress = (completed / toComplete) * 100;
+</script>
+
+<div class="completed">
+    {completed} / {toComplete} completed
+    <div class="progress">
+        <div class="progress-bar" style="width: {progress}%;"></div>
+    </div>
+</div>
+
+<style>
+    .completed {
+        color: var(--accent-light);
+    }
+    .progress {
+        height: 25px;
+        border: 2px solid var(--accent-light);
+        margin: 0 auto;
+        border-radius: .9em;
+    }
+    .progress-bar {
+        border-radius: 1em;
+        height: 21px; 
+        background-color: var(--accent-light);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,34 @@
+<script>
+    import ProgressBar from "../lib/components/ProgressBar.svelte";
+</script>
+
 <main>
     <a href="/async">
         <section>
             <h2>Async/Await Puzzles</h2>
             <p>Test your knowledge of async/await</p>
+            <ProgressBar completed=2 toComplete=5 />
         </section>
     </a>
     <a href="/filepath">
         <section>
             <h2>Filepath Puzzles</h2>
             <p>Think you know your way around filepaths?</p>
+            <ProgressBar completed=1 toComplete=5 />
         </section>
     </a>
     <a href="/css">
         <section>
             <h2>CSS Selector Puzzles</h2>
             <p>Can you select the right elements with CSS?</p>
+            <ProgressBar completed=4 toComplete=5 />
         </section>
     </a>
     <a href="/">
         <section>
             <h2>JS Array Method Puzzles</h2>
             <p>Master JavaScript array methods</p>
+            <ProgressBar completed=5 toComplete=5 />
         </section>
     </a>
 </main>
