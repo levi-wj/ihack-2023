7bdd4b35c478d326927f2039eb122d6027305f2d
Levi Jones <leviwill.j@gmail.com>
2023-10-20 22:04:41 -0600

    Got level picking working

---
+++ src/lib/components/LevelPicker.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let levels = [];
+    export let setCurLevel;
+</script>
+
+<select on:change={e => setCurLevel(e.target.value) }>
+    {#each levels as level, i}
+        <option value={i}>{level.title}</option>
+    {/each}
+</select>
--- src/routes/async/+page.svelte
+++ src/routes/async/+page.svelte
@@ -1,5 +1,26 @@
 <script>
-    let level = 1;
+    import Level1 from "./levels/Level1.svelte";
+    import Level2 from "./levels/Level2.svelte";
+    import Level3 from "./levels/Level3.svelte";
+    import Level4 from "./levels/Level4.svelte";
+    import Level5 from "./levels/Level5.svelte";
+    import LevelPicker from "../../lib/components/LevelPicker.svelte";
+    
+    const levels = [
+        { title: 'Level 1: Get data from API', component: Level1}, 
+        { title: 'Level 2: Get data from API 2', component: Level2},
+        { title: 'Level 3: Data from multiple sources', component: Level3},
+        { title: 'Level 4: Async in loops', component: Level4},
+        { title: 'Level 5: Convert promises to async', component: Level5},
+    ];
+
+    let curLevel = 0;
+    
+    function setCurLevel(level) {
+        curLevel = level;
+    }
 </script>
 
-<h2>This page is about Async/await</h2>
+<h2>Async/await puzzles</h2>
+<LevelPicker {levels} {setCurLevel}></LevelPicker>
+<svelte:component this={levels[curLevel].component} />
